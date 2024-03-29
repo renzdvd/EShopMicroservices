27d65f88398aff21b50d544504720e6864b288d8
Henry John David <renz.dvd@gmail.com>
2024-03-29 23:45:59 +0100

    Implementation of Mediatr into CreateProductHandler

--- src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,10 +1,17 @@
-﻿namespace Catalog.API.Products.CreateProduct
+﻿using MediatR;
+
+namespace Catalog.API.Products.CreateProduct
 {
-    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
+        : IRequest<CreateProductResult>;
     public record CreateProductResult(Guid Id);
 
-    public class CreateProductHandler
+    public class CreateProductHandler : IRequestHandler<CreateProductCommand, CreateProductResult>
     {
-
+        public Task<CreateProductResult> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            // Business logic to create a product
+            throw new NotImplementedException();
+        }
     }
 }
