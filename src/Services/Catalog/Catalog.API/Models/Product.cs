eff35e9570147597b6790613e1af6de9a2265a76
Henry John David <renz.dvd@gmail.com>
2024-03-29 23:31:41 +0100

    Created Product Model

--- src/Services/Catalog/Catalog.API/Models/Product.cs
+++ src/Services/Catalog/Catalog.API/Models/Product.cs
@@ -3,10 +3,10 @@
     public class Product
     {
         public Guid Id { get; set; }
-        public string Name { get; set; } = default;
+        public string Name { get; set; } = default!;
         public List<string> Category { get; set; } = new();
-        public string Description { get; set; } = default;
-        public string ImageFile { get; set; } = default;
+        public string Description { get; set; } = default!;
+        public string ImageFile { get; set; } = default!;
         public decimal Price {  get; set; }
 
 
