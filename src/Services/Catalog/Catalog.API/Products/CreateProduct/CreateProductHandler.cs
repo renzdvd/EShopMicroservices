b2cf3b1fb015b804b86d07bba8659f39e22e74eb
Henry John David <renz.dvd@gmail.com>
2024-03-29 23:32:16 +0100

    CreateProduct Features

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Products.CreateProduct
+{
+    public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price);
+    public record CreateProductResult(Guid Id);
+
+    public class CreateProductHandler
+    {
+
+    }
+}
