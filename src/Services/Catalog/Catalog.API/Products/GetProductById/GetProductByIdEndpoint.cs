4ba8489e4d6e6e98a0e62950f7eff3479ac7f7c5
Henry John David <renz.dvd@gmail.com>
2024-03-31 00:27:34 +0100

    Fix GetProductByIdEndpoint

--- src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProductById/GetProductByIdEndpoint.cs
@@ -8,7 +8,7 @@ namespace Catalog.API.Products.GetProductById
     {
         public void AddRoutes(IEndpointRouteBuilder app)
         {
-            app.MapGet("/products/{id", async (Guid id, ISender sender) =>
+            app.MapGet("/products/{id}", async (Guid id, ISender sender) =>
             {
                 var result = await sender.Send(new GetProductByIdQuery(id));
                 var response = result.Adapt<GetProductByIdResponse>();
