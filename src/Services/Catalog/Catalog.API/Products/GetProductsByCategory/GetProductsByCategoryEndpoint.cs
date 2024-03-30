1e6559c2fc581d0ad3b242e5adc1db8bb5d09820
Henry John David <renz.dvd@gmail.com>
2024-03-31 00:54:34 +0100

    GetProductByCategory feature

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductsByCategory/GetProductsByCategoryEndpoint.cs
@@ -0,0 +1,25 @@
+﻿
+namespace Catalog.API.Products.GetProductsByCategory
+{
+    //public record GetProductsByCategoryRequest();
+    public record GetProductsByCategoryResponse(IEnumerable<Product> Products);
+
+    public class GetProductsByCategoryEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/products/category/{category}", async (string category, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductsByCategoryQuery(category));
+                var products = result.Adapt<GetProductsByCategoryResponse>();
+
+                return Results.Ok(products);
+            })
+            .WithName("GetProductsByCategory")
+            .Produces<GetProductsByCategoryResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .WithSummary("Get Products by category")
+            .WithDescription("Get Products by category");
+        }
+    }
+}
