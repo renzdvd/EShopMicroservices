cb44477cc2a645bfabcdb212cf5e7413666ad9fb
Henry John David <renz.dvd@gmail.com>
2024-04-12 01:28:11 +0200

    added pagination for GetProducts

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,7 +1,9 @@
 ﻿
+using Marten.Pagination;
+
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10);
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10) : IQuery<GetProductsResult>;
     public record GetProductsResult(IEnumerable<Product> Products);
 
     internal class GetProductsQueryHandler(IDocumentSession session)
@@ -9,7 +11,8 @@ namespace Catalog.API.Products.GetProducts
     {
         public async Task<GetProductsResult> Handle(GetProductsQuery query, CancellationToken cancellationToken)
         {
-            var products = await session.Query<Product>().ToListAsync(cancellationToken);
+            var products = await session.Query<Product>()
+                .ToPagedListAsync(query.PageNumber ?? 1, query.PageSize ?? 10, cancellationToken);
 
             return new GetProductsResult(products);
         }
