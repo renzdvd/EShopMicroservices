c04baf2cd3946ba062314e77cc995633d0fe89b5
Henry John David <renz.dvd@gmail.com>
2024-04-12 01:00:27 +0200

    Create initial data

--- src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
+++ src/Services/Catalog/Catalog.API/Products/GetProducts/GetProductsHandler.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace Catalog.API.Products.GetProducts
 {
-    public record GetProductsQuery() : IQuery<GetProductsResult>;
+    public record GetProductsQuery(int? PageNumber = 1, int? PageSize = 10);
     public record GetProductsResult(IEnumerable<Product> Products);
 
     internal class GetProductsQueryHandler(IDocumentSession session)
