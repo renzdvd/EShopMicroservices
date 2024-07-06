2d4f8d38eae286a8d1a4783341307ae47778b27a
Henry John David <renz.dvd@gmail.com>
2024-07-06 18:08:11 +0200

    created CachedbasketRepository

---
+++ src/Services/Basket/Basket.API/Data/CachedBasketRepository.cs
@@ -0,0 +1,21 @@
+﻿namespace Basket.API.Data
+{
+    public class CachedBasketRepository(IBasketRepository repository)
+        : IBasketRepository
+    {
+        public async Task<ShoppingCart> GetBasket(string userName, CancellationToken cancellationToken)
+        {
+            return await repository.GetBasket(userName, cancellationToken);
+        }
+
+        public async Task<ShoppingCart> StoreBasket(ShoppingCart basket, CancellationToken cancellationToken)
+        {
+            return await repository.StoreBasket(basket, cancellationToken);
+        }
+
+        public Task<bool> DeleteBasket(string userName, CancellationToken cancellationToken = default)
+        {
+            return repository.DeleteBasket(userName, cancellationToken);
+        }
+    }
+}
--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,3 +1,5 @@
+using BuildingBlocks.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container
@@ -15,10 +17,12 @@ builder.Services.AddMarten(opts =>
     opts.Schema.For<ShoppingCart>().Identity(x => x.Username);
 }).UseLightweightSessions();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
 app.MapCarter();
+app.UseExceptionHandler(options => { });
 
 app.Run();
