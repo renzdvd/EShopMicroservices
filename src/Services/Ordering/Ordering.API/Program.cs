28e4f667dc1d7ecd885fcadb952585459dc6af97
Henry John David <renz.dvd@gmail.com>
2024-11-23 01:33:03 +0100

    Auto-migrate Ordering entities on application startup

--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,6 +1,7 @@
 using Ordering.API;
 using Ordering.Application;
 using Ordering.Infrastructure;
+using Ordering.Infrastructure.Data.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +15,11 @@ var app = builder.Build();
 
 
 // Configure the HTTP request pipeline
+app.UseApiServices();
 
+if (app.Environment.IsDevelopment())
+{
+    await app.InitializeDatabaseAsync();
+}
 
 app.Run();
